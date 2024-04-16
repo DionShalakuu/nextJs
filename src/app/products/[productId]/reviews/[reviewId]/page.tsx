f1c3e28355be29884441b24e25d77c57093d3312
dionsh <dionsh@CAC-DZHS-Z030>
2024-04-16 17:00:41 +0200

    Not found route

--- src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import {notFound} from "next/navigation"
 
 const ReviewDetail = ({params}:{
     params:{
@@ -6,9 +7,15 @@ const ReviewDetail = ({params}:{
         reviewId:string;
     }
 }) => {
+  if(parseInt(params.reviewId)> 1000){
+    notFound();
+  }
   return (
     <div>Review {params.productId} for product {params.reviewId}</div>
   )
 }
+
+
+
 
 export default ReviewDetail
